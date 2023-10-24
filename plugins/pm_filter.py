c67737722e668057a008e6836d466d0413c28dac
Rindo55 <77073061+Rindo55@users.noreply.github.com>
2023-10-24 08:50:46 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -101,7 +101,7 @@ async def next_page(bot, query):
     temp.FILES_IDS[key] = files
     if settings['button']:
         btn = []
-        segs = [f"[🔖{get_size(file.file_size)} - {file.file_name}]((https://t.me/anime_data_bot?start=file_{file.file_id}))\n({file.caption}.split("\n")[0])" for file in files]
+        segs = [f"[🔖{get_size(file.file_size)} - {file.file_name}]((https://t.me/anime_data_bot?start=file_{file.file_id}))\n(({file.caption}).splitlines()[0])" for file in files]
         segs1 = "\n\n".join(segs)
     else:
         btn = []
@@ -1283,7 +1283,7 @@ async def auto_filter(client, msg, spoll=False):
     BUTTONS[key] = search
     if settings["button"]:
         btn = []
-        segs = [f"[🔖{get_size(file.file_size)} - {file.file_name}]((https://t.me/anime_data_bot?start=file_{file.file_id}))\n({file.caption}.split("\n")[0])" for file in files]
+        segs = [f"[🔖{get_size(file.file_size)} - {file.file_name}]((https://t.me/anime_data_bot?start=file_{file.file_id}))\n(({file.caption}).splitlines()[0])" for file in files]
         segs1 = "\n\n".join(segs)
     else:
         btn = []
