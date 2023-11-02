78304a82502601d4f29b7c7237ac3e557667abb7
Rindo55 <77073061+Rindo55@users.noreply.github.com>
2023-11-02 21:30:22 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -63,6 +63,56 @@ async def give_filter(client, message):
         else:
             return await message.reply_text(f"<b>👋 𝖧𝖾𝗒 {message.from_user.mention} \n📁 {str(total_results)} 𝖱𝖾𝗌𝗎𝗅𝗍𝗌 𝖺𝗋𝖾 𝖿𝗈𝗎𝗇𝖽 𝖿𝗈𝗋 𝗒𝗈𝗎𝗋 𝗊𝗎𝖾𝗋𝗒 {search}.\n\nKindly ask movies and series here ⬇\n@blaster_arena & @blaster_movies</b>")
 
+client = MongoClient('mongodb+srv://anidata:animehevc@cluster0.xa2h6hh.mongodb.net/?retryWrites=true&w=majority')
+db = client['telegram_bot']
+collection = db['query_limits']
+@app.on_message(filters.command(['querystatus']))
+def query_status(_, message: Message):
+    user_id = message.from_user.id
+    query_limit = 10
+
+    # Check if user entry exists in the collection
+    user_entry = collection.find_one({'user_id': user_id})
+    if user_entry:
+        queries_left = user_entry['queries_left']
+        message.reply(f"You have {queries_left} queries left for today.")
+    else:
+        message.reply(f"You have {query_limit} queries left for today.")
+@app.on_message(filters.text & ~filters.command)
+def handle_message(_, message: Message):
+    user_id = message.from_user.id
+    query_limit = 10
+
+    # Check if user entry exists in the collection
+    user_entry = collection.find_one({'user_id': user_id})
+
+    # If user entry exists, check if limit reached
+    if user_entry:
+        queries_left = user_entry['queries_left']
+        last_query_time = user_entry['last_query_time']
+
+        # Calculate time difference from last query
+        time_diff = datetime.now() - last_query_time
+
+        # If more than 24 hours passed, reset the limit
+        if time_diff > timedelta(hours=24):
+            queries_left = query_limit
+
+        # If limit reached, send message and return
+        if queries_left <= 0:
+            message.reply("You have reached today's limit of 10 queries.")
+            return
+
+        # Update the query count and last query time
+        collection.update_one(
+            {'user_id': user_id},
+            {'$set': {'queries_left': queries_left - 1, 'last_query_time': datetime.now()}}
+        )
+    else:
+        # Create new user entry
+        collection.insert_one(
+            {'user_id': user_id, 'queries_left': query_limit - 1, 'last_query_time': datetime.now()}
+        )
 @Client.on_message(filters.private & filters.text & filters.incoming)
 async def pv_filter(client, message):
     kd = await global_filters(client, message)
